e0a067595f3652c97bd9a9f82762dfdfcf8dfcff
Jake Johnson <jdjhc4@umsystem.edu>
2021-03-11 12:31:11 -0600

    Added tabs & will fix them later!

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict'
+
+function openTab(tabType, tabName) {
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablink[i].className = tablinks[i].className.replace("active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    tabType.currentTarget.className += " active";
+
+}
